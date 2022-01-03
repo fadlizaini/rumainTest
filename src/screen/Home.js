7d128edca1c13aaabffe69d5c3bddeb7f318a65d
fadlizaini <fadli.zaini.abdullah@gmail.com>
2022-01-03 08:34:30 +0700

    sort

--- src/screen/Home.js
+++ src/screen/Home.js
@@ -8,21 +8,18 @@ import {
   FlatList,
   Alert,
   RefreshControl,
+  Modal,
+  ToastAndroid,
 } from 'react-native';
-import {
-  ICON_FILTER,
-  ICON_SEARCH,
-  SAMPLE_IMAGE,
-  BED,
-  FLOOR_PLAN,
-  BATHROOM,
-} from '../images';
+import {ICON_FILTER, ICON_SEARCH, BED, FLOOR_PLAN, BATHROOM} from '../images';
 import style from '../style';
 import http from '../http';
 import {getHome} from '../services/service';
 
 export default function Home(props) {
   const [data, setData] = useState([]);
+  const [sortType, setSortType] = useState(null);
+  const [visible, setVisible] = useState(false);
   const [page, setPage] = useState(1);
   const [refreshing, setRefreshing] = useState(false);
 
@@ -30,9 +27,10 @@ export default function Home(props) {
     onLoad();
   }, []);
 
-  function onLoad() {
+  function onLoad(ppage = page) {
+    console.log('ppage', ppage);
     setRefreshing(true);
-    getHome(page)
+    getHome(ppage)
       .then(response => {
         setRefreshing(false);
 
@@ -40,21 +38,57 @@ export default function Home(props) {
           x => x.categories[0].id === 118,
         );
 
-        setPage(page + 1);
-        if (page === 1) {
-          setData(filteredResponse);
+        setPage(ppage + 1);
+        if (ppage === 1) {
+          setData(
+            sortType === null
+              ? filteredResponse
+              : sortResponse(filteredResponse),
+          );
+          console.log(filteredResponse);
         } else {
           const mergeData = data.concat(filteredResponse);
-          setData(mergeData);
+          setData(sortType === null ? mergeData : sortResponse(mergeData));
+          console.log(mergeData);
         }
       })
 
       .catch(e => {
         setRefreshing(false);
-
         console.log('error getHome', e);
-        Alert.alert('Error', 'failed to load data');
+        if (ppage === 1) {
+          Alert.alert('Error', 'failed to load data');
+        } else {
+          ToastAndroid.show('cant load more data', ToastAndroid.SHORT);
+        }
       });
+  }
+
+  function sortResponse(data) {
+    switch (sortType) {
+      case 'asc':
+        data.sort((a, b) => (a.name > b.name ? 1 : -1));
+        return data;
+
+      case 'desc':
+        data.sort((a, b) => (a.name < b.name ? 1 : -1));
+        return data;
+
+      case 'new':
+        data.sort(function (a, b) {
+          return new Date(b.date_created) - new Date(a.date_created);
+        });
+        return data;
+
+      case 'old':
+        data.sort(function (a, b) {
+          return new Date(a.date_created) - new Date(b.date_created);
+        });
+        return data;
+
+      default:
+        return null;
+    }
   }
 
   return (
@@ -62,23 +96,25 @@ export default function Home(props) {
       <View style={style.headerInputWrapper}>
         <Image source={ICON_SEARCH} />
         <TextInput placeholder="Location" style={{flex: 1, marginLeft: 8}} />
-        <View style={style.filterIconWrapper}>
+        <TouchableOpacity
+          style={style.filterIconWrapper}
+          onPress={() => setVisible(true)}>
           <Image source={ICON_FILTER} />
-        </View>
+        </TouchableOpacity>
       </View>
       <FlatList
         refreshControl={
           <RefreshControl
             refreshing={refreshing}
-            onRefresh={() => {
-              setPage(1) && onLoad();
+            onRefresh={async () => {
+              onLoad(1);
             }}
           />
         }
         contentContainerStyle={{alignItems: 'center', paddingBottom: 100}}
         data={data}
         ListEmptyComponent={<Text>Tidak ada data</Text>}
-        onEndReached={() => onLoad()}
+        onEndReached={() => onLoad(page)}
         onEndReachedThreshold={0.1}
         style={{width: '100%', flex: 1}}
         renderItem={({item}) => (
@@ -108,6 +144,75 @@ export default function Home(props) {
         )}
         keyExtractor={item => item.id}
       />
+      <Modal visible={visible} transparent={true}>
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: 'rgba(0,0,0,0.4)',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <View
+            style={{
+              borderRadius: 16,
+              backgroundColor: 'white',
+              alignItems: 'center',
+              paddingHorizontal: 20,
+              paddingVertical: 30,
+            }}>
+            <Text style={{marginTop: 10}}>Sort</Text>
+            <View style={{marginLeft: 10}}>
+              <TouchableOpacity
+                onPress={() => setSortType(sortType === 'asc' ? null : 'asc')}
+                style={[
+                  {paddingVertical: 10},
+                  sortType === 'asc' && {backgroundColor: 'gray'},
+                ]}>
+                <Text style={sortType === 'asc' && {color: 'white'}}>
+                  name A-Z
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => setSortType(sortType === 'desc' ? null : 'desc')}
+                style={[
+                  {paddingVertical: 10},
+                  sortType === 'desc' && {backgroundColor: 'gray'},
+                ]}>
+                <Text style={sortType === 'desc' && {color: 'white'}}>
+                  name Z-A
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => setSortType(sortType === 'new' ? null : 'new')}
+                style={[
+                  {paddingVertical: 10},
+                  sortType === 'new' && {backgroundColor: 'gray'},
+                ]}>
+                <Text style={sortType === 'new' && {color: 'white'}}>
+                  date newest
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => setSortType(sortType === 'old' ? null : 'old')}
+                style={[
+                  {paddingVertical: 10},
+                  sortType === 'old' && {backgroundColor: 'gray'},
+                ]}>
+                <Text style={sortType === 'old' && {color: 'white'}}>
+                  date oldest
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => {
+                  setVisible(false);
+                  onLoad(1);
+                }}>
+                <Text>Apply</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
